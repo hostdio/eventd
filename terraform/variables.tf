4e779948e7cb3dd184b65519d569dc2cb00c1581
Ken Grønnbeck <kengroenn@gmail.com>
2017-12-24 14:54:00 +0100

    Ignore cloud sql for now

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,6 +2,6 @@ variable "google_cloud_project_id" {
   type = "string"
 }
 
-variable "google_cloud_sql_admin_user_password" {
-  type = "string"
-}
+# variable "google_cloud_sql_admin_user_password" {
+#   type = "string"
+# }
